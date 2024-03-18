9abc15b012100e9fb5521f21861714d4bfd6ea5b
migue.centellas@gmail.com <migue.centellas@gmail.com>
2024-03-18 16:54:01 -0300

    Use of endpoint 'waypoints' and ListWayEndpoints

--- src/main/java/org/accenture/Main.java
+++ src/main/java/org/accenture/Main.java
@@ -2,13 +2,19 @@ package org.accenture;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import org.accenture.entities.responses.AcceptContractResponse;
+import org.accenture.entities.responses.ListWaypointsResponse;
 import org.accenture.entities.responses.RegisterNewAgentResponse;
 import org.accenture.entities.responses.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
@@ -48,5 +54,27 @@ public class Main {
         }
         AcceptContractResponse dataAcceptContractResponse = mapper.convertValue(body.getData(), AcceptContractResponse.class);
         System.out.println(dataAcceptContractResponse.getContract().getId());
+
+        String[] headquarter = dataAcceptContractResponse.getAgent().getHeadquarters().split("-");
+        var headquarterPart = headquarter[0] + "-" + headquarter[1];
+
+        response = Unirest.get("https://api.spacetraders.io/v2/systems/{systemSymbol}/waypoints")
+                .routeParam("systemSymbol", headquarterPart)
+                .queryString("type", "ENGINEERED_ASTEROID")
+                .asString();
+
+        body = mapper.readValue(response.getBody(), ResponseBody.class);
+        if (body.getError() != null) {
+            System.out.println(body.getError().getMessage());
+            return;
+        }
+        JsonNode nodes = body.getData();
+        List<ListWaypointsResponse> listOfWayPoints = new ArrayList();
+        for(JsonNode jnode: nodes){
+            ListWaypointsResponse listWaypointsResponse = mapper.convertValue(jnode,ListWaypointsResponse.class);
+            listOfWayPoints.add(listWaypointsResponse);
+        }
+        System.out.println("Primer Lista de traits - tamaño: "+listOfWayPoints.get(0).getTraits().length);
+
     }
 }
