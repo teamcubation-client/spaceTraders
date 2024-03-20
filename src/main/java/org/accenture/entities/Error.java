216339c2c341951e3211fa9e7944da71cfb30dae
Gaston Areco <gaston.areco@teamcubation.com>
2024-03-20 11:26:18 -0300

    Added more dynamic typing to the error data attribute in the response body

--- src/main/java/org/accenture/entities/Error.java
+++ src/main/java/org/accenture/entities/Error.java
@@ -1,10 +1,11 @@
 package org.accenture.entities;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Getter;
 
 @Getter
 public class Error {
     private String message;
     private String code;
-    private String data;
+    private JsonNode data;
 }
