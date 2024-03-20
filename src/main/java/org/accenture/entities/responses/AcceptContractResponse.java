cdecfc97f0ca0c0ea53a1a40f3927ecaed3c12d2
agostinaietri <agostinaietri@gmail.com>
2024-03-20 12:51:40 -0300

    removed setter import

--- src/main/java/org/accenture/entities/responses/AcceptContractResponse.java
+++ src/main/java/org/accenture/entities/responses/AcceptContractResponse.java
@@ -1,7 +1,6 @@
 package org.accenture.entities.responses;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.accenture.entities.Agent;
 import org.accenture.entities.Contract;
 
--- src/main/java/org/accenture/entities/responses/RegisterNewAgentResponse.java
+++ src/main/java/org/accenture/entities/responses/RegisterNewAgentResponse.java
@@ -1,7 +1,6 @@
 package org.accenture.entities.responses;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 import org.accenture.entities.*;
 
