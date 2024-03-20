e2201f6a52e1ae7156fa19f8c7d99365f41c3f55
Adrian <adrian.marin@accenture.com>
2024-03-20 15:49:06 -0300

    Step 3 adjustments: Added getType method to Point class. Sleep times removed.

--- src/main/java/org/accenture/entities/Point.java
+++ src/main/java/org/accenture/entities/Point.java
@@ -9,6 +9,9 @@ public class Point {
     private String systemSymbol;
     private int x;
     private int y;
+    public String getType(){
+        return this.type.name();
+    }
 
     private enum Type {
         PLANET, GAS_GIANT, MOON, ORBITAL_STATION, JUMP_GATE, ASTEROID_FIELD, ASTEROID, ENGINEERED_ASTEROID,
