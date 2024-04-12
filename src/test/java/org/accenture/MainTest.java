030c8bc39709931567e024a90887d1b0d991697e
Gaston Areco <gaston.areco@teamcubation.com>
2024-04-12 09:49:33 -0300

    Updated printed assertions / Added Unirest get mocking

--- src/test/java/org/accenture/MainTest.java
+++ src/test/java/org/accenture/MainTest.java
@@ -1,10 +1,8 @@
 package org.accenture;
 
-import kong.unirest.HttpRequestWithBody;
-import kong.unirest.HttpResponse;
-import kong.unirest.RequestBodyEntity;
-import kong.unirest.Unirest;
+import kong.unirest.*;
 import org.accenture.mocks.MockResponses;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
@@ -12,46 +10,62 @@ import org.mockito.MockedStatic;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.nio.charset.Charset;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class MainTest {
 
-    private static String consoleOutput;
-
-    private HttpRequestWithBody setMockUnirest(String responseBody, boolean hasBody) {
-        RequestBodyEntity requestBodyEntity = mock(RequestBodyEntity.class);
+    private enum RestMethods {
+        POST, GET
+    }
+    private HttpRequest setMockUnirest(String responseBody, RestMethods method, boolean hasBody) {
         HttpResponse<String> httpResponse = mock(HttpResponse.class);
-        HttpRequestWithBody httpRequestWithBody = mock(HttpRequestWithBody.class);
-        when(httpRequestWithBody.routeParam(anyString(), anyString())).thenReturn(httpRequestWithBody);
-        when(httpRequestWithBody.header(anyString(), anyString())).thenReturn(httpRequestWithBody);
-        if (hasBody) {
-            when(httpRequestWithBody.body(anyString())).thenReturn(requestBodyEntity);
-            when(requestBodyEntity.asString()).thenReturn(httpResponse);
-        } else {
-            when(httpRequestWithBody.asString()).thenReturn(httpResponse);
+        if (method.equals(RestMethods.POST)) {
+            HttpRequestWithBody httpRequestWithBody = mock(HttpRequestWithBody.class);
+            RequestBodyEntity requestBodyEntity = mock(RequestBodyEntity.class);
+            when(httpRequestWithBody.routeParam(anyString(), anyString())).thenReturn(httpRequestWithBody);
+            when(httpRequestWithBody.header(anyString(), anyString())).thenReturn(httpRequestWithBody);
+            if (hasBody) {
+                when(httpRequestWithBody.body(anyString())).thenReturn(requestBodyEntity);
+                when(requestBodyEntity.asString()).thenReturn(httpResponse);
+            } else {
+                when(httpRequestWithBody.asString()).thenReturn(httpResponse);
+            }
+            when(httpResponse.getBody()).thenReturn(responseBody);
+            return httpRequestWithBody;
         }
-        when(httpResponse.getBody()).thenReturn(responseBody);
-        return httpRequestWithBody;
+        if (method.equals(RestMethods.GET)) {
+            GetRequest getRequest = mock(GetRequest.class);
+            when(getRequest.routeParam(anyString(), anyString())).thenReturn(getRequest);
+            when(getRequest.queryString(anyMap())).thenReturn(getRequest);
+            when(getRequest.header(anyString(), anyString())).thenReturn(getRequest);
+            when(getRequest.asString()).thenReturn(httpResponse);
+            when(httpResponse.getBody()).thenReturn(responseBody);
+            return getRequest;
+        }
+        return null;
     }
 
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
     @BeforeEach
-    void setUp() {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream out = new PrintStream(byteArrayOutputStream);
-        System.setOut(out);
-        consoleOutput = byteArrayOutputStream.toString(Charset.defaultCharset());
-        out.close();
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
     }
 
     @Test
     void whenApiError_ThenThrowExceptionError() {
         try (MockedStatic<Unirest> mockedStatic = mockStatic(Unirest.class)) {
-            HttpRequestWithBody httpRequestWithBody = setMockUnirest(MockResponses.responseError, true);
+            HttpRequest httpRequest = setMockUnirest(MockResponses.responseError, RestMethods.POST, true);
             mockedStatic.when(Unirest::config).thenCallRealMethod();
-            mockedStatic.when(() -> Unirest.post("/register")).thenReturn(httpRequestWithBody);
+            mockedStatic.when(() -> Unirest.post("/register")).thenReturn(httpRequest);
             assertThrows(Error.class, () -> Main.main(new String[]{}));
             mockedStatic.verify(() -> Unirest.post("/register"));
         }
@@ -61,10 +75,20 @@ public class MainTest {
     public void whenRegisterNewAgent_ThenAcceptContract() throws IOException, InterruptedException {
         try (MockedStatic<Unirest> mockedStatic = mockStatic(Unirest.class)) {
             mockedStatic.when(Unirest::config).thenCallRealMethod();
-            HttpRequestWithBody httpRequestWithBodyRegisterNewAgent = setMockUnirest(MockResponses.responseRegisterNewAgent, true);
-            mockedStatic.when(() -> Unirest.post("/register")).thenReturn(httpRequestWithBodyRegisterNewAgent);
-            HttpRequestWithBody httpRequestWithBodyAcceptContract = setMockUnirest(MockResponses.responseError, false);
-            mockedStatic.when(() -> Unirest.post("/my/contracts/{contractId}/accept")).thenReturn(httpRequestWithBodyAcceptContract);
+            HttpRequest httpRequestRegisterNewAgent = setMockUnirest(MockResponses.responseRegisterNewAgent, RestMethods.POST, true);
+            mockedStatic.when(() -> Unirest.post("/register")).thenReturn(httpRequestRegisterNewAgent);
+            HttpRequest httpRequestAcceptContract = setMockUnirest(MockResponses.responseError, RestMethods.POST, false);
+            mockedStatic.when(() -> Unirest.post("/my/contracts/{contractId}/accept")).thenReturn(httpRequestAcceptContract);
+            try {
+                Main.main(new String[]{});
+            } catch (Error e) {
+                assertEquals("API Error", e.getMessage());
+            }
+            assertTrue(outputStreamCaptor.toString().contains("Token: 123"));
+            mockedStatic.verify(() -> Unirest.post("/register"));
+            mockedStatic.verify(() -> Unirest.post("/my/contracts/{contractId}/accept"));
+        }
+    }
 
             HttpRequestWithBody httpRequestWithBodyListWaypoints = setMockUnirest(MockResponses.responseListWaypointsInSystem, true);
             mockedStatic.when(() -> Unirest.get("/systems/{systemSymbol}/waypoints")).thenReturn(httpRequestWithBodyListWaypoints);
